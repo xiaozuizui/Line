14aceed68eeeb3691f408a5de4371ff48cdf28ac
littlemm <785897146@qq.com>
2018-04-11 18:34:22 +0800

    圆的光栅化算法

--- WpfLine/Parameter_Equation_Circle.cs
+++ WpfLine/Parameter_Equation_Circle.cs
@@ -10,19 +10,20 @@ namespace WpfLine
     {
         public Parameter_Equation_Circle(float _x, float _y, float R) : base(_x, _y, R)
         {
-            int x = Convert.ToInt32(Math.Round(R + 0.5));
+            int x = Convert.ToInt32(Math.Round(R + 0.5));//起点为（R,0）
             int y = 0;
 
-            double thta = 0;
+            double theta = 0;
+
             while (y <= x)//绘制八分之一,右上圆
             {
                 DrawCirclePoints(x, y);
-                x = Convert.ToInt32( R* Math.Cos(thta));
-                y = Convert.ToInt32(R * Math.Sin(thta));
-                thta += 1.0/72.0*Math.PI;
+                x = Convert.ToInt32( R* Math.Cos(theta));
+                y = Convert.ToInt32(R * Math.Sin(theta));
+                theta += 1.0/72.0*Math.PI; //当圆的半径过大时，需要更小的θ ,来保证精度
              }
 
-            Transfer();
+            Transfer();//圆自身坐标系转换为世界坐标系
 
         }
     }
